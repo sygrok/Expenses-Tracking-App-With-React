5c911b935a748c122e3ca58b4950e75e9d3b19cb
Berkay Anduv <sygrok@hotmail.com>
2023-08-20 08:46:02 +0300

    StylingForExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>#280.00</div>
+        <div className="expense-item__price">#280.00</div>
       </div>
     </div>
   );
