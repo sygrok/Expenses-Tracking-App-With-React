1c85989402bd866743331a481dfb45ab292db9d1
Berkay Anduv <sygrok@hotmail.com>
2023-08-21 05:21:17 +0300

    PropsAlternative

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -13,3 +13,20 @@ function ExpenseItem(props) {
 }
 
 export default ExpenseItem;
+
+//Props Altarnate
+// import "./ExpenseItem.css";
+
+// function ExpenseItem({ title, date, amount }) {
+//   return (
+//     <div className="expense-item">
+//       <div>{date.toISOString()}</div>
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <div className="expense-item__price">${amount}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ExpenseItem;
