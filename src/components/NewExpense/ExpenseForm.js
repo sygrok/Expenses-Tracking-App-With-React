ccd8894d25db424956bcfd432d14af3e2a3efb60
Berkay Anduv <sygrok@hotmail.com>
2023-08-24 03:04:15 +0300

    SharedHandlerFunction

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,57 @@
 import "./ExpenseForm.css";
-import React from "react";
+import React, { useState } from "react";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier === "title") {
+      setEnteredTitle(value);
+    } else if (identifier === "date") {
+      setEnteredDate(value);
+    } else {
+      setEnteredAmount(value);
+    }
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2024-01-01" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2024-01-01"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
@@ -26,3 +62,47 @@ function ExpenseForm() {
 }
 
 export default ExpenseForm;
+
+//Shared Handler Function ****
+
+// const inputChangeHandler = (identifier, value) => {
+//   if (identifier === "title") {
+//     setEnteredTitle(value);
+//   } else if (identifier === "date") {
+//     setEnteredDate(value);
+//   } else {
+//     setEnteredAmount(value);
+//   }
+// };
+
+{
+  /* <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={(event) => inputChangeHandler('title',event.target.value)} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={(event) => inputChangeHandler('amount',event.target.value)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2024-01-01"
+            onChange={(event) => inputChangeHandler('date',event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form> */
+}
